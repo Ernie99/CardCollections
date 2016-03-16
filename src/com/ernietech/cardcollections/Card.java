bb92c29659e88a5db49c3102617b3fa383649199
Ernie <ernie@ernietech.com>
2016-03-15 23:06:59 -0400

    Implement card and card collection

---
+++ src/com/ernietech/cardcollections/Card.java
@@ -0,0 +1,58 @@
+package com.ernietech.cardcollections;
+
+/**
+ * Created by Ernie on 3/13/2016.
+ */
+public abstract class Card {
+
+    public static void main(String [] args){ // main just for testing! junit is better but this is quick
+        System.out.println("testing card class and subclass");
+        Card c1 = new FrenchCard(Suit.SPADE, Face.ACE); // ace of spades
+        Card c2 = new FrenchCard(Suit.SPADE, Face.ACE);
+        Card c3 = new FrenchCard(Suit.HEART, Face.TWO); // duce of hearts
+        System.out.println(c1);
+        System.out.println(c1.equals(c2));
+        System.out.println(c1.equals(c3));
+    }
+
+
+    public static class FrenchCard extends Card{ // "French" card because wiki says the French invented the "modern" playing card
+        private final Suit suit;
+        private final Face face;
+
+        FrenchCard(Suit s, Face f){
+            suit = s;
+            face = f;
+        }
+
+        public Suit getSuit() {
+            return suit;
+        }
+
+        public Face getFace() {
+            return face;
+        }
+
+        @Override
+        public boolean equals(Object obj) { // maybe throws class cast exception?
+//            return super.equals(obj);
+            if((((FrenchCard)obj).getFace()==getFace())  && (((FrenchCard)obj).getSuit()==getSuit())){
+                return true;
+            }
+            return false;
+        }
+
+        @Override
+        public String toString() {
+            String s = " Suit: " + getSuit().name() + ", Face: " + getFace().name();
+            return s;
+        }
+    }
+
+    public enum Suit {
+        CLUB, DIAMOND, HEART, SPADE;
+    }
+    public enum Face{
+        ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, JACK, QUEEN, KING
+    }
+}
---
+++ src/com/ernietech/cardcollections/CardCollection.java
@@ -0,0 +1,164 @@
+package com.ernietech.cardcollections;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Random;
+
+import static java.util.Collections.shuffle;
+
+/**
+ * Created by Ernie on 3/13/2016.
+ */
+
+/**
+ * abstract parent for all card collections (standard decks, hands, multi decks, etc)
+ * @param <T> type of card used
+ *
+ * NOTE: we are using "T extends Card" for future renovation
+ */
+public abstract class CardCollection <T extends Card> implements Iterable<T> {
+
+    public static void main(String [] args){ // main just for testing!
+        Deck<Card.FrenchCard> testDeck = new Deck<Card.FrenchCard>();
+        System.out.println(testDeck);
+        System.out.println(testDeck.drawRandom());
+        System.out.println(testDeck);
+    }
+
+    private List<T> cards = new ArrayList<>();
+
+    @Override
+    public Iterator<T> iterator() {
+        return cards.iterator(); //TODO this could be bad, not tested or used for now
+        //also this may expose the inner collection too much
+        //iterator returned is not typed
+    }
+
+    // private for now, not sure if we should expose the list api,
+    // or call methods with our own "convenience" api
+    public int getSize(){
+        return cards.size();
+    }
+
+    @Override
+    public String toString() {
+//        return super.toString();
+        String s = ""; // yes a string builder is better
+        for(T card : cards){
+            s += "[" + card.toString() + "]";
+        }
+        return s;
+    }
+
+    protected List<T> getCards(){ // not part of the public api!
+        return cards;
+    }
+
+    // we only want to draw from a deck, so no method to add cards!
+    // no method to see what cards are in the deck
+    // Deck is initialized to 52 cards
+
+    /**
+     * base class that represents deck of 52 cards
+     *
+     * API designed to mimic actions a dealer would perform on a deck
+     * No methods to "peek", or add cards back.
+     * When deck is exhausted create a new one. Perhaps add more
+     * functions to add card, reshuffle, etc.. maybe useful for unusual
+     * card games that I have not played.
+     *
+     * @param <T> type of card used
+     */
+    public static class Deck <T extends Card>  extends CardCollection<T>{ // single deck
+        /**
+         * initialized to a FULL DECK
+         */
+        public Deck(){
+            super();
+            List<T> cards = super.getCards();
+            buildMiniDeck();
+        }
+
+        /**
+         * draws random card and removes from deck
+         * @return card from deck
+         * MODIFIES super.cards
+         */
+        public T drawRandom(){ // make generator static and use const seed for repeatability, good for testing, bad for cheating
+            List<T> cards = super.getCards();
+            Random randGen = new Random();
+            int randIndex = randGen.nextInt(super.getSize());
+            T randomPick = cards.get(randIndex);
+            cards.remove(randIndex);
+            return randomPick;
+        }
+
+        // helper to insert 52 unique cards
+        // TODO this is bad, must change! we are making new FrenchCards and not T
+        // TODO will use builder or factory pattern
+        private void buildDeck(){
+            List<T> cards = super.getCards();
+            for(Card.Suit s : Card.Suit.values()){
+                for(Card.Face f : Card.Face.values()){
+                    //do we need this cast? alternative ? think this is  good way
+                   cards.add((T) new Card.FrenchCard(s,f));
+                }
+            }
+//            shuffle(cards);, we don't need since we draw at random
+        }
+
+        //do not use! used to make small deck for testing
+        private void buildMiniDeck(){
+            List<T> cards = super.getCards();
+            cards.add((T) new Card.FrenchCard(Card.Suit.SPADE,Card.Face.TWO));
+            cards.add((T) new Card.FrenchCard(Card.Suit.SPADE,Card.Face.THREE));
+            cards.add((T) new Card.FrenchCard(Card.Suit.SPADE,Card.Face.FOUR));
+            cards.add((T) new Card.FrenchCard(Card.Suit.SPADE,Card.Face.FIVE));
+        }
+    }
+
+    /**
+     *  base class that represents hand of cards, also used as a pool
+     *
+     * @param <T> type of card used
+     */
+    public static class Hand <T extends Card>  extends CardCollection<T>{
+        /**
+         * initialized to an EMPTY HAND
+         */
+        Hand(){
+            super();
+        }
+
+        /**
+         * add card to hand
+         * @param c card to add
+         * MODIFIES super.cards
+         */
+        public void addCard(Card c){
+            super.getCards().add((T) c);
+        }
+
+        /**
+         * draw card from hand
+         * @param s suit of card to draw
+         * @param f face of card to draw
+         * @return T card from hand
+         * @throws UnsupportedOperationException when hand does not contain card, unchecked exception!
+         * MODIFIES super.cards
+         */
+        public T drawCard(Card.Suit s, Card.Face f) throws UnsupportedOperationException{
+            List<T> cards = super.getCards();
+            //TODO this is bad, must change! we are making new FrenchCards and not T
+            T testCard = (T) new Card.FrenchCard(s,f);
+            int index = -1;
+            index = cards.indexOf(testCard);
+            if(index == -1){
+                throw new UnsupportedOperationException("tried ti use card that is not in hand");
+            }
+            cards.remove(index);
+            return testCard; // we are returning test card, maybe safer to return actual card
+        }
+    }
+}
