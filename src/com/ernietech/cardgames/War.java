84c08a8f74ed0a989814f1a457abcf23a334cfc6
Ernie Homsy <ernest.l.homsy.ctr@mail.mil>
2016-03-17 15:18:06 -0400

    break-up code into more methods

--- src/com/ernietech/cardgames/War.java
+++ src/com/ernietech/cardgames/War.java
@@ -16,58 +16,12 @@ import java.util.List;
  * Just using this as a test for decks and cards, so all code is static, for simplicity
  */
 public class War {
+    static int repeatedWarCounter = 0; // our only public
+
     public static void main(String [] args){
         //main deck that we start with
-        CardCollection.Deck<Card.FrenchCard> deck = new CardCollection.Deck<Card.FrenchCard>();
-
-        //player hands
-        CardCollection.Hand<Card.FrenchCard> playerOneHand = new CardCollection.Hand<>();
-        CardCollection.Hand<Card.FrenchCard> playerTwoHand = new CardCollection.Hand<>();
-
-        //player point piles
-        CardCollection.Hand<Card.FrenchCard> playerOnePile = new CardCollection.Hand<>();
-        CardCollection.Hand<Card.FrenchCard> playerTwoPile = new CardCollection.Hand<>();
-
-        //deal two hands
-        while(deck.getSize() !=0 ){ // shouldn't throw because we have 2 players and even deck size
-            playerOneHand.addCard(deck.drawRandom());
-            playerTwoHand.addCard(deck.drawRandom());
-        }
-//        System.out.println(playerOneHand);
-//        System.out.println(playerTwoHand);
-
-        //todo BIG BIG BIG todo, we are repeating the logic in draw war, fix this!
-        while(playerOneHand.getSize()!=0){ // really we should check both hands for empty, but since both players are drawing at the same time, this is ok
-            Card.FrenchCard c1 = playerOneHand.drawRandom(); //// TODO again, draw random may not be best
-            Card.FrenchCard c2 = playerTwoHand.drawRandom();
-
-            String played = "player 1: " + c1 + " player 2: " + c2;
-            System.out.println(played);
-            String result = "";
-
-            int weight1 = getCardWeight(c1);
-            int weight2 = getCardWeight(c2);
-
-            if(weight1>weight2){
-                result = "player 1 wins";
-                playerOnePile.addCard(c1); // put both cards in player 1 pile
-                playerOnePile.addCard(c2);
-            } else if(weight2>weight1){
-                result= "player 2 wins";
-                playerTwoPile.addCard(c1); // put both cards in player 2 pile
-                playerTwoPile.addCard(c2);
-            } else{
-//                result = "WAR";
-                System.out.println("WAR");
-                List<Card.FrenchCard> warPile = new ArrayList<>();
-                int warWinner = drawWar(playerOneHand, playerTwoHand, warPile);
-                if(warWinner==1){
-                    addToPile(playerOnePile, warPile);
-                }else if(warWinner==2){
-                    addToPile(playerTwoPile, warPile);
-                }
-            }
-            System.out.println(result);
+        for(int i = 0; i < 100; i++){
+            playGame();
         }
         System.exit(0);
     }
@@ -76,9 +30,10 @@ public class War {
     private static int drawWar(CardCollection.Hand<Card.FrenchCard> playerOneHand, CardCollection.Hand<Card.FrenchCard> playerTwoHand, List<Card.FrenchCard> pile){
 //        List<Card.FrenchCard> warPile = new ArrayList<>();
         // we are using a *regular* java collection to hold cards // todo, think about this as it relates to design decisions
+        repeatedWarCounter ++;
         int cardsToWar = 3;
-        if(playerOneHand.getSize() < 4){ // if we are short on cards while in a war
-            cardsToWar = playerOneHand.getSize() - 1;
+        if(playerOneHand.getSize() < 2){ // if we are short on cards while in a war
+            cardsToWar = playerOneHand.getSize() - 2;
         }
         for(int i=0; i < cardsToWar; i++){
             pile.add(playerOneHand.drawRandom());
@@ -102,9 +57,67 @@ public class War {
         } else{
 //            result = "another war!";
             //todo not the best println, also repeats code
-            System.out.println("another war, tie on " + playerOneWarCard + " " + playerTwoWarCard);// todo use more sensible logic println ordering
+            System.out.println("another war #" + repeatedWarCounter + " tie on " + playerOneWarCard + " " + playerTwoWarCard);// todo use more sensible logic println ordering
             System.out.println("WEIGHTS= " + weight1 + " " + weight2);
             return drawWar(playerOneHand, playerTwoHand, pile);
+        }
+    }
+
+    private static void playGame(){
+        CardCollection.Deck<Card.FrenchCard> deck = new CardCollection.Deck<Card.FrenchCard>();
+
+        //player hands
+        CardCollection.Hand<Card.FrenchCard> playerOneHand = new CardCollection.Hand<>();
+        CardCollection.Hand<Card.FrenchCard> playerTwoHand = new CardCollection.Hand<>();
+
+        //player point piles
+        CardCollection.Hand<Card.FrenchCard> playerOnePile = new CardCollection.Hand<>();
+        CardCollection.Hand<Card.FrenchCard> playerTwoPile = new CardCollection.Hand<>();
+
+        //deal two hands
+        while(deck.getSize() !=0 ){ // shouldn't throw because we have 2 players and even deck size
+            playerOneHand.addCard(deck.drawRandom());
+            playerTwoHand.addCard(deck.drawRandom());
+        }
+//        System.out.println(playerOneHand);
+//        System.out.println(playerTwoHand);
+
+
+
+        //todo BIG todo, we are repeating the logic in draw war, fix code duplication!
+        while(playerOneHand.getSize() != 0){ // really we should check both hands for empty, but since both players are drawing at the same time, this is ok
+            Card.FrenchCard c1 = playerOneHand.drawRandom(); //// TODO again, draw random may not be best
+            Card.FrenchCard c2 = playerTwoHand.drawRandom();
+
+            String played = "player 1: " + c1 + " player 2: " + c2;
+            System.out.println(played);
+            String result = "";
+
+            int weight1 = getCardWeight(c1);
+            int weight2 = getCardWeight(c2);
+
+            if(weight1>weight2){
+                result = "player 1 wins";
+                playerOnePile.addCard(c1); // put both cards in player 1 pile
+                playerOnePile.addCard(c2);
+                repeatedWarCounter = 0;
+            } else if(weight2>weight1){
+                result= "player 2 wins";
+                playerTwoPile.addCard(c1); // put both cards in player 2 pile
+                playerTwoPile.addCard(c2);
+                repeatedWarCounter = 0;
+            } else{
+//                result = "WAR";
+                System.out.println("WAR");
+                List<Card.FrenchCard> warPile = new ArrayList<>();
+                int warWinner = drawWar(playerOneHand, playerTwoHand, warPile);
+                if(warWinner==1){
+                    addToPile(playerOnePile, warPile);
+                }else if(warWinner==2){
+                    addToPile(playerTwoPile, warPile);
+                }
+            }
+            System.out.println(result);
         }
 
     }
